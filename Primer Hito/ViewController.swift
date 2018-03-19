60c995b95ae43953f243e839feea3de5c133e6c2
albertogurpegui <alberto.gurpeguiramon@gmail.com>
2018-03-19 18:01:29 +0100

    Conexiones Avanzadas

--- Primer Hito/ViewController.swift
+++ Primer Hito/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet var btnLogear:UIButton?
     @IBOutlet var txtfUsuario:UITextField?
     @IBOutlet var txtfContraseña:UITextField?
+    @IBOutlet var txtfVConsola:UITextView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +24,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func accionBotonLogear(){
+        txtfVConsola?.text=String(format:"HOLA MUNDO USUARIO: %@ PASSWORD: %@", (txtfUsuario?.text)!, (txtfContraseña?.text)!)
+    }
+    
 
 }
 
