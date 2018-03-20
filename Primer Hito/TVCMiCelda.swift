7560cfb53f350a52f1e0ae6522cf426849482ac4
albertogurpegui <alberto.gurpeguiramon@gmail.com>
2018-03-20 13:16:09 +0100

    Creando celdas diferenciadas usando los métodos del Delegate

---
+++ Primer Hito/TVCMiCelda.swift
@@ -0,0 +1,24 @@
+//
+//  TVCMiCelda.swift
+//  Primer Hito
+//
+//  Created by ALBERTO GURPEGUI RAMÓN on 20/3/18.
+//  Copyright © 2018 ALBERTO GURPEGUI RAMÓN. All rights reserved.
+//
+
+import UIKit
+
+class TVCMiCelda: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
