2ada029564d1e41630f98d84dbc28a7187a9016f
albertogurpegui <alberto.gurpeguiramon@gmail.com>
2018-03-20 19:33:43 +0100

    DataHolder, variables globales y métodos/funciones globales, datos estáticos

---
+++ Primer Hito/DataHolder.swift
@@ -0,0 +1,13 @@
+//
+//  DataHolder.swift
+//  Primer Hito
+//
+//  Created by ALBERTO GURPEGUI RAMÓN on 20/3/18.
+//  Copyright © 2018 ALBERTO GURPEGUI RAMÓN. All rights reserved.
+//
+
+import UIKit
+
+class DataHolder: NSObject {
+
+}
