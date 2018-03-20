2ce97d208f196099e1164e2b896ca22ae5d0d065
albertogurpegui <alberto.gurpeguiramon@gmail.com>
2018-03-20 18:23:30 +0100

    Definiendo el Controlador de la Collection y asociando el DataSource y el Delegate

---
+++ Primer Hito/CVCMiCelda2.swift
@@ -0,0 +1,13 @@
+//
+//  CVCMiCelda2.swift
+//  Primer Hito
+//
+//  Created by ALBERTO GURPEGUI RAMÓN on 20/3/18.
+//  Copyright © 2018 ALBERTO GURPEGUI RAMÓN. All rights reserved.
+//
+
+import UIKit
+
+class CVCMiCelda2: UICollectionViewCell {
+    
+}
