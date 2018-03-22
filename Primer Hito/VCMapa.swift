2e2f90f4ab6cadf25f95b94ae1d1810585d5fa9e
albertogurpegui <alberto.gurpeguiramon@gmail.com>
2018-03-22 16:27:52 +0100

    MapKit + Pins (Map Pins) Annotations

--- Primer Hito/VCMapa.swift
+++ Primer Hito/VCMapa.swift
@@ -19,6 +19,15 @@ class VCMapa: UIViewController, MKMapViewDelegate{
         
         //miMapa?.delegate = self
         // Do any additional setup after loading the view.
+        
+        var coordTemp:CLLocationCoordinate2D = CLLocationCoordinate2D()
+        coordTemp.latitude = 40.4165000
+        coordTemp.longitude = -3.7025600
+        agregarPin(coordenada: coordTemp, titulo: " PIN1 ")
+        var coordTemp2:CLLocationCoordinate2D = CLLocationCoordinate2D()
+        coordTemp.latitude = 48.4165000
+        coordTemp.longitude = -4.7025600
+        agregarPin(coordenada: coordTemp2, titulo: " PIN2 ")
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,8 +35,11 @@ class VCMapa: UIViewController, MKMapViewDelegate{
         // Dispose of any resources that can be recreated.
     }
     
-    func localizacionActualizada(coordenada: CLLocationCoordinate2D) {
-        centralizarEnPosicion(coordenada: coordenada)
+    func agregarPin(coordenada:CLLocationCoordinate2D, titulo varTitulo:String) {
+        let annotation:MKPointAnnotation = MKPointAnnotation()
+        annotation.coordinate = coordenada
+        annotation.title = varTitulo
+        miMapa?.addAnnotation(annotation)
     }
     
     func centralizarEnPosicion(coordenada:CLLocationCoordinate2D) {
