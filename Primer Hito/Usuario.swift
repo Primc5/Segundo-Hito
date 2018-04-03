9233a177d6af4cce62f972b142fa29891efeafcb
albertogurpegui <alberto.gurpeguiramon@gmail.com>
2018-04-03 19:03:26 +0200

    Firebase DataBase + UITableView Parte2

---
+++ Primer Hito/Usuario.swift
@@ -0,0 +1,13 @@
+//
+//  Usuario.swift
+//  Primer Hito
+//
+//  Created by ALBERTO GURPEGUI RAMÓN on 3/4/18.
+//  Copyright © 2018 ALBERTO GURPEGUI RAMÓN. All rights reserved.
+//
+
+import UIKit
+
+class Usuario: NSObject {
+
+}
