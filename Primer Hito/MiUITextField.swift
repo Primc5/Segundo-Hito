fdfeb8f6e8568d14854afd3d925dd2ae3b703b45
albertogurpegui <alberto.gurpeguiramon@gmail.com>
2018-04-23 19:39:14 +0200

    iOS Swift Avanzado: Herencias

---
+++ Primer Hito/MiUITextField.swift
@@ -0,0 +1,21 @@
+//
+//  MiUITextField.swift
+//  Primer Hito
+//
+//  Created by ALBERTO GURPEGUI RAMÓN on 23/4/18.
+//  Copyright © 2018 ALBERTO GURPEGUI RAMÓN. All rights reserved.
+//
+
+import UIKit
+
+class MiUITextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
